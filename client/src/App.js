4793f2d16f22391c7352d7b25029e0ef1d445a8c
el bandingo <elbandingo@protonmail.com>
2022-03-16 00:00:07 -0230

    fixed a few bugs finally...got the routes working

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import NavBar from './components/Navbar';
 import './App.css';
 import { render } from "react-dom";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/screens/Home'
-//import Login from './components/screens/Login'
-//import Profile from './components/screens/Profile'
-//import Signup from './components/screens/Signup'
+import Login from './components/screens/Login'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
 
 function App() {
   return (
-    <NavBar />
+    <Router>
+      <NavBar />
+<Routes>
+  <Route exact path="/" element={<Home />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/signup" element={<Signup />} />
+  <Route path="/profile" element={<Profile />} />
+</Routes>
+</Router>
 
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,9 +6,9 @@ const NavBar = ()=> {
     <div className="nav-wrapper black">
       <a href="/" className="brand-logo">Logo</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/signup">sign up</a></li>
+        <li><a href="/profile">My Profile</a></li>
       </ul>
     </div>
   </nav>
